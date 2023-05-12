1469082cf33e22668f66f1f85172296540ce9683
ahashi16 <cnss2.meme@gmail.com>
2023-05-12 18:57:58 +0900

    Userモデル単体テスト修正

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -75,7 +75,46 @@ RSpec.describe User, type: :model do
       @user.valid?
       expect(@user.errors.full_messages).to include('Password is too short (minimum is 6 characters)')
      end
-    
+     it '英字のみのパスワードでは登録できない' do
+        @user.password = 'aaaaaaaa'
+        @user.password_confirmation = 'aaaaaaaa'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password is invalid")
+     end
+     it '数字のみのパスワードでは登録できない' do
+       @user.password ='11111111'
+        @user.password_confirmation = '11111111'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password is invalid")
+     end
+     it '全角文字を含むパスワードでは登録できない' do
+       @user.password = 'ああああああ'
+        @user.password_confirmation = 'ああああああ'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password is invalid")
+     end
+     it '姓（全角）に半角文字が含まれていると登録できない' do
+       @user.last_name = 'aaaa'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Last name is invalid")
+     end
+     it '名（全角）に半角文字が含まれていると登録できない' do
+       @user.first_name = 'aaaa'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("First name is invalid")
+     end
+     it '姓（カナ）にカタカナ以外の文字（平仮名・漢字・英数字・記号）が含まれていると登録できない' do
+       @user.last_name_kana="kana"
+       @user.valid?
+        expect(@user.errors.full_messages).to include("Last name kana is invalid")
+
+     end
+     it '名（カナ）にカタカナ以外の文字（平仮名・漢字・英数字・記号）が含まれていると登録できない' do
+      @user.first_name_kana="kana"
+       @user.valid?
+        expect(@user.errors.full_messages).to include("First name kana is invalid")
+     end
+     
     end
   end
 end
