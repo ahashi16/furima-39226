9465ca53a21bf94d2bc2d6e7213326e53330d5bb
ahashi16 <cnss2.meme@gmail.com>
2023-05-13 18:11:28 +0900

    商品出品機能を実装

---
+++ app/models/Genre.rb
@@ -0,0 +1,19 @@
+class Genre < ActiveHash::Base
+  self.data = [
+    { id: 1, name: '---' },
+    { id: 2, name: 'レディース' },
+    { id: 3, name: 'メンズ' },
+    { id: 4, name: 'ベビーキッズ' },
+    { id: 5, name: 'インテリア住まい小物' },
+    { id: 6, name: '本音楽ゲーム' },
+    { id: 7, name: 'おもちゃホビーグッズ' },
+    { id: 8, name: '家電スマホカメラ' },
+    { id: 9, name: 'スポーツレジャー' },
+    { id: 10, name: 'ハンドメイド' },
+    { id: 11, name: 'その他' },
+  ]
+
+  include ActiveHash::Associations
+  has_many :items
+
+  end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,20 @@
 class Item < ApplicationRecord
+  validates :product_name,presence: true
+  validates :description,presence: true
+  validates :user,presence: true
+  validates :genre_id, numericality: { other_than: 1 ,message: "can't be blank"},presence: true
+  validates :item_condition_id, numericality: { other_than: 1 ,message: "can't be blank"},presence: true
+  validates :shipping_fee_payer_id, numericality: { other_than: 1 ,message: "can't be blank"},presence: true
+  validates :shipping_area_id, numericality: { other_than: 1 ,message: "can't be blank"},presence: true
+  validates :shipping_day_id, numericality: { other_than: 1 ,message: "can't be blank"},presence: true
+  validates :price, numericality: { greater_than_or_equal_to: 300 ,less_than_or_equal_to:9999999}, presence: true
+  
   belongs_to :user
   has_one_attached :image
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :Genre
+  belongs_to :ItemCondition
+  belongs_to :ShippingFeePayer
+  belongs_to :ShippingArea
+  belongs_to :ShippingDay
 end
