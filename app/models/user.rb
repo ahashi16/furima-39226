ec9afe17224a5436fa1346bdd4840f4afc9992f9
ahashi16 <cnss2.meme@gmail.com>
2023-05-17 12:11:57 +0900

    商品詳細表示機能

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,12 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         validates :nickname, presence: true
-         validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥々ー]+\z/}
-         validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥々ー]+\z/}
-         validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
-         validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
-         validates :password,presence: true, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i }
-         validates :birth_day, presence: true
-         has_many :itens
+  validates :nickname, presence: true
+  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥々ー]+\z/ }
+  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥々ー]+\z/ }
+  validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
+  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
+  validates :password, presence: true, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
+  validates :birth_day, presence: true
+  has_many :itens
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,12 +2,12 @@ FactoryBot.define do
   factory :user do
     nickname { Faker::Name.name }
     email { Faker::Internet.free_email }
-    password {'1a' + Faker::Internet.password(min_length: 6)}
+    password { '1a' + Faker::Internet.password(min_length: 6) }
     password_confirmation { password }
-    first_name { "あいうえお" }
-    last_name { "かきくけこ" }
-    first_name_kana {"アイウエオ"}
-    last_name_kana{"カキクケコ"} 
+    first_name { 'あいうえお' }
+    last_name { 'かきくけこ' }
+    first_name_kana { 'アイウエオ' }
+    last_name_kana { 'カキクケコ' }
     birth_day { Faker::Date.backward }
   end
 end
