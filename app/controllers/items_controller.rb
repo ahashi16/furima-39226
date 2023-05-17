c821fefead8d5e8e6ed6a7c50da71e7cd408f7d4
ahashi16 <cnss2.meme@gmail.com>
2023-05-17 19:39:49 +0900

    修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,17 +1,16 @@
 class ItemsController < ApplicationController
+  before_action :set_item, only:[:show,:edit,:update]
+  before_action :authenticate_user!,only:[:new,:edit]
   def index
     @items = Item.order(created_at: :desc)
   end
 
   def new
     @item = Item.new
-    return if user_signed_in?
-
-    redirect_to root_path
   end
 
   def create
-    @item = Item.create(item_params)
+    @item = Item.new(item_params)
     if @item.save
       redirect_to root_path
     else
@@ -20,29 +19,20 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
-    unless user_signed_in?
-      redirect_to new_user_session_path
-      return
-    end
-
-    unless  @item.user_id == current_user.id
+   unless  @item.user_id == current_user.id
       redirect_to root_path 
       return
     end
     
-
-    unless @item.present?
+   unless @item.present?
       redirect_to root_path
     end
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to root_path
     else
@@ -55,5 +45,14 @@ class ItemsController < ApplicationController
   def item_params
     params.require(:item).permit(:product_name, :description, :genre_id, :item_condition_id, :shipping_fee_payer_id, :shipping_area_id,
                                  :shipping_day_id, :price, :image).merge(user_id: current_user.id)
+  end
+  def set_item
+    @item = Item.find(params[:id])
+  end
+  def authenticate_user!
+    unless user_signed_in?
+      redirect_to new_user_session_path
+      return
+    end
   end
 end
