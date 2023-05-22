83608836f004ce58e7fa4327786aa808f516d0a2
ahashi16 <cnss2.meme@gmail.com>
2023-05-22 20:53:33 +0900

    修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -14,7 +14,6 @@ class OrdersController < ApplicationController
     
     @item = Item.find(params[:item_id])
     @order_address=OrderAddress.new(order_params)
-    
     if @order_address.valid? 
       Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
      Payjp::Charge.create(
@@ -25,7 +24,8 @@ class OrdersController < ApplicationController
       @order_address.save
       return redirect_to root_path
     else
-      render 'index'
+      render :index
+      
     end
   end
   private
