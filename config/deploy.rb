70405a1dae0eed9fcb618b2209696651e28349fa
ahashi16 <cnss2.meme@gmail.com>
2023-06-12 14:47:05 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,7 +5,7 @@ lock '~> 3.1'
 set :application, 'furima-39226'
 
 # どのリポジトリからアプリをpullするかを指定する
-set :repo_url,  'git@github.com:ahashi16/furima-39226.git'
+set :repo_url,  'git@github.com:ahashi16/furima-39226.git', branch: 'main'
 
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
